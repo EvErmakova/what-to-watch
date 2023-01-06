dbf43e3075be6a7fac1481d4f3857cd31788b1bd
Евгения Ермакова <russkih.evgeniya92@gmail.com>
2023-01-06 11:11:07 +0400

    Добавит пропсы и propTypes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from "./components/app/app";
 
 ReactDOM.render(
-    <App />,
+    <App
+      movies={[`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows`, `Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`]}
+    />,
     document.querySelector(`#root`)
 );
