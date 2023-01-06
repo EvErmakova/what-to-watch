98b9ca75258581f36f1dc255a8b454ace91e3bc4
Евгения Ермакова <russkih.evgeniya92@gmail.com>
2023-01-06 11:33:16 +0400

    Добавит новые компоненты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
+import Catalog from "../catalog/catalog";
 
 const App = (props) => {
   const {movies} = props;
+
   return (
     <React.Fragment>
       <section className="movie-card">
@@ -62,61 +64,7 @@ const App = (props) => {
       </section>
 
       <div className="page-content">
-        <section className="catalog">
-          <h2 className="catalog__title visually-hidden">Catalog</h2>
-
-          <ul className="catalog__genres-list">
-            <li className="catalog__genres-item catalog__genres-item--active">
-              <a href="#" className="catalog__genres-link">All genres</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Comedies</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Crime</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Documentary</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Dramas</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Horror</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Kids & Family</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Romance</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Sci-Fi</a>
-            </li>
-            <li className="catalog__genres-item">
-              <a href="#" className="catalog__genres-link">Thrillers</a>
-            </li>
-          </ul>
-
-          <div className="catalog__movies-list">
-            {movies.map((movie, index) => (
-              <article className="small-movie-card catalog__movies-card" key={index}>
-                <div className="small-movie-card__image">
-                  <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg"
-                    alt={movie} width="280" height="175"/>
-                </div>
-                <h3 className="small-movie-card__title">
-                  <a className="small-movie-card__link" href="movie-page.html">{movie}</a>
-                </h3>
-              </article>
-            ))}
-          </div>
-
-          <div className="catalog__more">
-            <button className="catalog__button" type="button">Show more</button>
-          </div>
-        </section>
-
+        <Catalog movies={movies}/>
         <footer className="page-footer">
           <div className="logo">
             <a className="logo__link logo__link--light">
